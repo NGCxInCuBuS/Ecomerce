5c0f2f7fb5b7cc7f5654938db9cdd16adf386bba
NGCxInCuBuS <happyfella64@gmail.com>
2023-04-30 03:34:51 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,112 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Corgi Booty Buns",
+    price: 25,
+    colors: [
+      {
+        img: "./img/booty-removebg-preview.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Assorted Japanese Bread",
+    price: 15,
+    colors: [
+      {
+        img: "./img/Assorted Japanese Bread.jpg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Bento Cat",
+    price: 20,
+    colors: [
+      {
+        img: "./Img/Bento Cats.jpg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Pupper-Nickle Ham&Cheese",
+    price: 8,
+    colors: [
+      {
+        img: "./img/Ham and cheese.jpg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Japanese Milk Bread",
+    price: 12,
+    colors: [
+      {
+        img: "./img/Japanese Milk Bread.jpg",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
